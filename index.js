e148dde0052bca5bbe3abdf79711fd305fc71465
wirednkod <wirednkod@gmail.com>
2022-11-07 01:20:38 +0200

    Updates

--- index.js
+++ index.js
@@ -12,14 +12,16 @@ window.onload = () => {
     ui.showSyncing();
     void (async () => {
         try {
-            const { block: { block } } = await C.readBlock(C.westend).run();
+            const currentBlock = await C.readBlock(C.westend).run();
+            // (currentBlock).block.block.header.number)
             ui.showSynced();
-            ui.log(`${emojis.seedling} Chain is ready (powered by CAPI) - Reading at block #${parseInt(block.header.number, 0)}`, true);
+            ui.log(`${emojis.seedling} Chain is ready (powered by CAPI) - Reading at block #${parseInt(currentBlock.block.block.header.number, 0)}`, true);
             const chainName = await C.rpcCall(C.westend, "system_chain", []).select("result").run();
             const health = await C.rpcCall(C.westend, "system_health", []).select("result").run();
             const genesisHash = await C.rpcCall(C.westend, "chain_getBlockHash", [0]).select("result").run();
-            ui.log(`${emojis.info} Connected to ${chainName} with ${health.peers} peers.`);
+            ui.log(`${emojis.info} Connected to ${chainName}.`);
             ui.log(`${emojis.chequeredFlag} Genesis hash is ${genesisHash}`);
+            ui.log(`${emojis.stethoscope} Chain is syncing with ${health.peers}`);
             ui.log(`${emojis.newspaper} Subscribing to new block headers`);
             const rpc = C.rpcSubscription(C.westend, "chain_subscribeNewHead", [], () => (m) => ui.log(`${emojis.brick} New block #${parseInt(m.params.result.number, 0)} has hash ${m.params.result.parentHash}`));
             await C.run(rpc);
